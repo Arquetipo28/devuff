7bc3dc287bc191a9d51578844f71712fb7b2c4c9
Arquetipo28 <german.reynagaa@gmail.com>
2021-04-30 07:54:03 -0500

    feat: add initial pages

---
+++ src/components/HomeMenu.js
@@ -0,0 +1,18 @@
+import './HomeMenu.css';
+
+const HomeMenu = () => {
+  return (
+    <div class='Home-menu menu-size'>
+      <h2 class='title'>Arquetipo28</h2>
+      <ul class='posts-titles'>
+        <li class='post-title'>
+          <a class='post-link' target='_blank' href='https://arquetipo28.medium.com/using-google-drive-api-with-ruby-3f40e342439e'>
+            Using Google Drive api with Ruby
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+};
+
+export default HomeMenu;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+import HomeMenu from '../components/HomeMenu';
+
+const HomePage = () => {
+  return(
+    <>
+      <div className='wrapper'>
+        <HomeMenu />
+      </div>
+    </>
+  )
+}
+export default HomePage;
